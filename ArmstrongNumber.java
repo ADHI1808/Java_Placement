f15c883854a134cdc93ed628e3ceae435009aee1
Adhithyan S <s.adhithyan2004@gmail.com>
2024-11-19 11:49:02 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,32 +1,30 @@
+
 import java.util.Scanner;
 
-public class ArmstrongNumber {
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        String len;
+        int a, temp, rem = 0,pow;
+        double sum = 0; // Use double since Math.pow returns double
 
-        // Input the number
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        System.out.println("Enter number:");
+        a = s.nextInt();
+        temp = a;
+        len = Integer.toString(a);
+        pow = len.length();
 
-        // Find if it is an Armstrong number
-        if (isArmstrong(number)) {
-            System.out.println(number + " is an Armstrong number.");
+        while (temp > 0) {
+            rem = temp % 10; // Extract the last digit
+            temp = temp / 10; // Remove the last digit
+            sum = sum + Math.pow(rem, pow); // Calculate digit^length
+        }
+
+        if (sum == a) {
+            System.out.println("It is an Armstrong Number");
         } else {
-            System.out.println(number + " is not an Armstrong number.");
+            System.out.println("Not an Armstrong Number");
         }
-    }
-
-    public static boolean isArmstrong(int num) {
-        int originalNumber = num;
-        int sum = 0;
-        int numberOfDigits =Integer.toString.length(); //to conver int to string
-
-        while (num > 0) {
-            int digit = num % 10;
-            sum += Math.pow(digit, numberOfDigits);
-            num /= 10;
-        }
-
-        return sum == originalNumber;
+        s.close();
     }
 }
