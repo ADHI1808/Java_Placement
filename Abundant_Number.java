f3b3a226c3b9c8ffa2a770e0e86a4397838f92a0
Adhithyan S <s.adhithyan2004@gmail.com>
2024-11-19 11:58:44 +0530

    Create Abundant_Number.java

---
+++ Abundant_Number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a,sum=0;
+        System.out.println("Enter Number");
+        a=s.nextInt();
+        for(int i=1;i<a/2;i++){
+            if(a%i==0){
+                sum+=i;
+            }
+        }
+        if(sum>a){
+            System.out.println("Yes Abundunt Number");//also known as Exclusive Number
+        }
+        else{
+            System.out.println("Not a Abundunt Number");
+        }
+        s.close();
+    }
+}
