23136d889ac614a7835100ed7122f38f15022680
Adhithyan S <s.adhithyan2004@gmail.com>
2024-11-19 12:26:49 +0530

    Update AutomorpicNumber.java

--- AutomorpicNumber.java
+++ AutomorpicNumber.java
@@ -12,10 +12,14 @@ class Main {
 
         while (a > 0) {
             if (a % 10 != sq % 10) {
-                isAutomorphic = false; // Mark as not Automorphic
+                isAutomorphic = false; // if 1st digit itself doesn't match then
+                //Mark as not Automorphic and ends  the loop
                 break;
             }
-            a /= 10;
+            a /= 10; //if 2digit number then after checking 2 number
+            //it will became 0,hence loop will end
+            //ex for a=25,it will check 5 then comes ro 2 if its ture,then further
+            //if 2 matches then 2/10=0 since while(a>0) loop will end 
             sq /= 10;
         }
 
